948e91e47a70c5c76aa3c63537e69b8e9d129a9e
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-07-25 18:14:52 -0300

    <test>(products): 100% controller

--- tests/unit/product/productController.test.js
+++ tests/unit/product/productController.test.js
@@ -55,4 +55,61 @@ describe('User Controller Test', () => {
       });
     });
   });
+  describe('02. Getting products by ID', () => {
+    describe("When the request is successfully completed", () => {
+      const request = {};
+      const response = {};
+      const result = { data: [{ id: 1 }], status: status.OK };
+      const paramID = 1;
+
+      before(async () => {
+        request.params = sinon.stub().returns(paramID);
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
+
+        sinon.stub(productService, "getByID").resolves(result);
+      });
+
+      after(async () => {
+        productService.getByID.restore();
+      });
+
+      it("a code 200 OK status is called", async () => {
+        await productController.getByID(request, response);
+        expect(response.status.calledWith(status.OK)).to.equal(true);
+      });
+      it("the right data is called", async () => {
+        await productController.getByID(request, response);
+        expect(response.json.calledWith(result.data)).to.equal(true);
+      });
+    });
+    describe("When the request fails", () => {
+      const request = {};
+      const response = {};
+      const next = () => {};
+      const result = {
+        message: "No data avaliable",
+        status: status.NO_CONTENT,
+      };
+      const paramID = 5;
+
+      before(async () => {
+        request.params = sinon.stub().returns(paramID);
+        sinon.stub(productService, "getByID").resolves(result);
+      });
+
+      after(async () => {
+        productService.getByID.restore();
+      });
+
+      it("an error is throwed", async () => {
+        try {
+          await productController.getByID(request, response, next);
+        } catch (error) {
+          expect(error.message).to.equal(result.message);
+          expect(error.status).to.equal(result.status);
+        }
+      });
+    });
+  });
 });
